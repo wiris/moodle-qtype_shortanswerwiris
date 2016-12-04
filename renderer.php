28fb84ed4b3662d8b907c566796c7c13031ea9d6
AndresMelo <Andres.Torres@blackboard.com>
2016-12-04 17:52:35 -0500

    Suggestion - code review, style changes
    Some style changes

--- renderer.php
+++ renderer.php
@@ -18,13 +18,12 @@ defined('MOODLE_INTERNAL') || die();
 require_once($CFG->dirroot . '/question/type/shortanswer/renderer.php');
 require_once($CFG->dirroot . '/question/type/wq/renderer.php');
 
-class qtype_shortanswerwiris_renderer extends qtype_wq_renderer{
+class qtype_shortanswerwiris_renderer extends qtype_wq_renderer {
     public function __construct(moodle_page $page, $target) {
         parent::__construct(new qtype_shortanswer_renderer($page, $target), $page, $target);
     }
 
-    public function formulation_and_controls(question_attempt $qa,
-            question_display_options $options) {
+    public function formulation_and_controls(question_attempt $qa, question_display_options $options) {
 
         $question = $qa->get_question();
         $currentanswer = $qa->get_last_qt_var('answer');
@@ -62,14 +61,12 @@ class qtype_shortanswerwiris_renderer extends qtype_wq_renderer{
         $result .= html_writer::start_tag('div', array('class' => 'ablock'));
         $result .= $this->auxiliar_cas();
         $result .= html_writer::tag('label', get_string('answer', 'qtype_shortanswer',
-                   html_writer::tag('span', $input, array('class' => 'answer'))),
-                   array('for' => $inputattributes['id']));
+                html_writer::tag('span', $input, array('class' => 'answer'))), array('for' => $inputattributes['id']));
         $result .= html_writer::end_tag('div');
 
         if ($qa->get_state() == question_state::$invalid) {
             $result .= html_writer::nonempty_tag('div',
-                    $question->get_validation_error(array('answer' => $currentanswer)),
-                    array('class' => 'validationerror'));
+                    $question->get_validation_error(array('answer' => $currentanswer)), array('class' => 'validationerror'));
         }
         $result .= $this->add_javascript();
         $result .= $this->lang();
