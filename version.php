9322811f418850cc401bf596368ad9435cd53376
Manuel Cagigas <manuel@wiris.com>
2017-07-10 09:03:42 +0200

    3.54.0 Stable version

--- version.php
+++ version.php
@@ -16,11 +16,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017051000;
-$plugin->release = '3.52.0.1036';
+$plugin->version = 2017070900;
+$plugin->release = '3.54.0.1024';
 $plugin->requires = 2011060313;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'qtype_shortanswerwiris';
 $plugin->dependencies = array (
-     'qtype_wq' => 2017051000
+     'qtype_wq' => 2017070900
 );
