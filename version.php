e0467eb2ae5e5114ed041816483c23ac151c89fd
Manuel <manuel@wiris.com>
2017-11-27 11:25:23 +0100

    2017112700 development version

--- version.php
+++ version.php
@@ -16,11 +16,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2017112600;
+$plugin->version = 2017112700;
 
 $plugin->requires = 2011060313;
 $plugin->maturity = MATURITY_BETA;
 $plugin->component = 'qtype_shortanswerwiris';
 $plugin->dependencies = array (
-     'qtype_wq' => 2017112600
+     'qtype_wq' => 2017112700
 );
