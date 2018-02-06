a8b7261724386f1f7961299c5967bd2833e4251d
Manuel <manuel@wiris.com>
2018-02-06 15:20:18 +0100

    2018020600 Stable version

--- version.php
+++ version.php
@@ -17,9 +17,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2018020600;
-
+$plugin->release = '3.61.0.1034';
 $plugin->requires = 2011060313;
-$plugin->maturity = MATURITY_BETA;
+$plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'qtype_shortanswerwiris';
 $plugin->dependencies = array (
      'qtype_wq' => 2018020600
