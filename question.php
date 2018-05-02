155662b5512a86dd1fcfb32afa0440b418d152a7
Jenkins <jenkins@wiris.com>
2018-05-03 00:39:27 +0200

    2018050300 development version

--- question.php
+++ question.php
@@ -247,6 +247,26 @@ class qtype_shortanswerwiris_question extends qtype_wq_question
         return $text;
     }
 
+    public function is_complete_response(array $response) {
+        if (array_key_exists('answer', $response) && $this->is_empty_mathml($response['answer'])) {
+            return false;
+        }else{
+           return parent::is_complete_response($response);
+        }
+    }
+
+    public function is_gradable_response(array $response) {
+        return $this->is_complete_response($response);
+    }
+
+    private function is_empty_mathml($string) {
+        $mathml = simplexml_load_string($string);
+        if (gettype($mathml)!= 'boolean' && $mathml->getName() == 'math' && count($mathml->children()) == 0) {
+            return true;
+        }
+        return false;
+    }
+
     private function is_text_answer() {
         if (!empty($this->parent)) {
             return true;
