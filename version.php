f6861c08ba348ec424987caa118fe3c99f8b8bd1
Manuel Cagigas <sedras@gmail.com>
2018-11-09 13:11:55 +0100

    3.70.0 stable version

--- version.php
+++ version.php
@@ -16,11 +16,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018091900;
-$plugin->release = '3.68.0.1048';
+$plugin->version = 2018110900;
+$plugin->release = '3.70.0.1049';
 $plugin->requires = 2011060313;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'qtype_shortanswerwiris';
 $plugin->dependencies = array (
-     'qtype_wq' => 2018091900
+     'qtype_wq' => 2018110900
 );
