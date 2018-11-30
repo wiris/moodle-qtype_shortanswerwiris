028411038714dd469629ce196005f1eca297118b
Jenkins <jenkins@wiris.com>
2018-11-30 22:01:42 +0100

    2018113000 development version

--- version.php
+++ version.php
@@ -16,11 +16,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018112900;
+$plugin->version = 2018113000;
 
 $plugin->requires = 2011060313;
 $plugin->maturity = MATURITY_BETA;
 $plugin->component = 'qtype_shortanswerwiris';
 $plugin->dependencies = array (
-     'qtype_wq' => 2018112900
+     'qtype_wq' => 2018113000
 );
