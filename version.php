4592df03de5c144c7db5ecf814b0e739baa4860e
Pol Torrent i Soler <pol@wiris.com>
2020-12-09 17:38:29 +0100

    v3.78.4

--- version.php
+++ version.php
@@ -16,11 +16,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020111000;
-$plugin->release = '3.78.3';
+$plugin->version = 2020120900;
+$plugin->release = '3.78.4';
 $plugin->requires = 2011060313;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'qtype_shortanswerwiris';
 $plugin->dependencies = array (
-     'qtype_wq' => 2020111000
+     'qtype_wq' => 2020120900
 );
