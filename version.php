f7337a003ec35f38e9bdecba14d68f0194668b81
Pol Torrent <pol.torrent@wiris.com>
2021-02-12 10:59:45 +0100

    Updates version to v4.2.3

--- version.php
+++ version.php
@@ -16,11 +16,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2020121600;
-$plugin->release = '4.2.0';
+$plugin->version = 2021021200;
+$plugin->release = '4.2.3';
 $plugin->requires = 2011060313;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_RC;
 $plugin->component = 'qtype_shortanswerwiris';
 $plugin->dependencies = array (
-     'qtype_wq' => 2020121600
+     'qtype_wq' => 2021021200
 );
