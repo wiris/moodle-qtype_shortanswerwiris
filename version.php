672fee85df0f69a0e4df0fe3db6e2dc0f2857765
pol-at-wiris <66125018+pol-at-wiris@users.noreply.github.com>
2021-05-06 17:07:37 +0200

    Update version.php

--- version.php
+++ version.php
@@ -16,11 +16,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021042100;
-$plugin->release = '4.2.6';
+$plugin->version = 2021050700;
+$plugin->release = '4.2.7';
 $plugin->requires = 2011060313;
 $plugin->maturity = MATURITY_RC;
 $plugin->component = 'qtype_shortanswerwiris';
 $plugin->dependencies = array (
-     'qtype_wq' => 2021042100
+     'qtype_wq' => 2021050700
 );
