1a84175e3887cdb0a14ca8d0c408f266f33bebb4
marc-vila-at-wiris <marc.vila@wiris.com>
2021-06-11 10:32:46 +0200

    Typo, Grunt and Moodle Codde Checker fixes

--- tests/behat/behat_shortanswerwiris_base.php
+++ tests/behat/behat_shortanswerwiris_base.php
@@ -34,11 +34,11 @@ class behat_shortanswerwiris_base extends behat_base {
 
     /**
      * Checks for an existing readonly input.
-     * 
+     *
      * @Then I should have a readonly input.
      * @throws Exception Thrown if not found.
      */
-    public function i_should_have_a_readonly_inpuy() {
+    public function i_should_have_a_readonly_input() {
         $session = $this->getSession();
         $readonly = $session->getPage()->find('css', '.wrs_readOnly');
         if (empty($readonly)) {
