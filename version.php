de4ba25c08fe3d971fa7bb59bc22583ff99823a1
Pol Torrent i Soler <pol.torrent@wiris.com>
2021-09-13 09:38:15 +0200

    Bumps version.php to 4.3.0

--- version.php
+++ version.php
@@ -16,11 +16,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2021050700;
-$plugin->release = '4.2.7';
+$plugin->version = 2021091300;
+$plugin->release = '4.3.0';
 $plugin->requires = 2011060313;
-$plugin->maturity = MATURITY_RC;
+$plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'qtype_shortanswerwiris';
 $plugin->dependencies = array (
-     'qtype_wq' => 2021050700
+     'qtype_wq' => 2021091300
 );
