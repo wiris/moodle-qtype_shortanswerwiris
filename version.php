24726aaafbece5844d19f3a636a2936a2f57fff9
Pol Torrent i Soler <66125018+ptorrent-at-wiris@users.noreply.github.com>
2023-09-22 00:19:25 +0200

    chore: update version to 4.9.0

--- version.php
+++ version.php
@@ -16,11 +16,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023050202;
+$plugin->version = 2023092200;
 $plugin->requires = 2015111600; // Moodle 3.0.
-$plugin->release = '4.8.2';
+$plugin->release = '4.9.0';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'qtype_shortanswerwiris';
 $plugin->dependencies = array (
-     'qtype_wq' => 2023050202
+     'qtype_wq' => 2023092200
 );
