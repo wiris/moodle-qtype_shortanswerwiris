4318ee723b1a8d1fac475c5899cfc5444a5a1888
Pol Torrent i Soler <pol.torrent@wiris.com>
2024-09-30 16:30:56 +0200

    chore: bump version to v4.11.4

--- version.php
+++ version.php
@@ -16,11 +16,11 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024032203;
+$plugin->version = 2024032204;
 $plugin->requires = 2015111600; // Moodle 3.0.
-$plugin->release = '4.11.3';
+$plugin->release = '4.11.4';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'qtype_shortanswerwiris';
 $plugin->dependencies = array(
-     'qtype_wq' => 2024032203
+     'qtype_wq' => 2024032204
 );
